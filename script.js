cbba9f190bce409015fa6385feaf8acdd2e7f107
Rafael Cirino <rafael.rel.q@gmail.com>
2025-06-11 09:48:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ send.addEventListener('click', function(event) {
     event.preventDefault();
     const password = pass.value
     const e_mail = user.value
-    if(e_mail === 'admin@fortes.com.br' || 'cooperativas@fortes.com.br' && password === '123456'){
+    if((e_mail === 'admin@fortes.com.br' || e_mail === 'cooperativas@fortes.com.br') && password === '123456'){
     alert('Login Succed')
     }else{
         alert('Login Failed')
